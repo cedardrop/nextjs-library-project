62b9e1b00e88584b590308d1f870daa1e8ccec64
2301322014 <stu2301322014@uni-plovdiv.bg>
2025-09-29 13:48:57 +0300

    Corrected some typos

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main style={{padding: '2rem', textAlign: 'center'}}>
       <h1> My Library</h1>
-      <p> Welocme to my first Next.js project</p>
+      <p> Welcome to my first Next.js project</p>
       </main>
   );
 }
